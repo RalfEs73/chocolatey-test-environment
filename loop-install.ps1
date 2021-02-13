0bda0a850d2f9ee0db2230cb200e92e038b81323
RalfEs73 <ralf@essbaumer.de>
2021-02-13 23:18:52 +0100

    Update loop-install.ps1

--- loop-install.ps1
+++ loop-install.ps1
@@ -1,8 +1,8 @@
-$package = "choco install cloudberrybackup --version=7.0.0.648"
+$package = "cloudberrybackup --version=7.0.0.648"
 
 For ($i=1; $i -lt 5; $i++) {
 	choco install $package
 	Start-Sleep 1
-	choco install $package
+	choco uninstall $package
 	Start-Sleep 1	
 	}

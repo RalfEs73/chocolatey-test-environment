62238ab283551fe46b8193fb3128f6f7433c8e19
RalfEs73 <ralf@essbaumer.de>
2021-02-13 22:57:08 +0100

    Create test-install.ps1

---
+++ test-install.ps1
@@ -0,0 +1,18 @@
+# iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/RalfEs73/win10_reinstall/master/win10_reinstall.ps1'))
+
+New-Item -Path "C:\Temp\ChocoCache" -ItemType directory -Force | Out-Null
+$cache = "--cacheLocation=C:\Temp\ChocoCache"
+
+$CurrentUser = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name
+$CurrentUserName = $CurrentUser.split("\")[1]
+
+Install-PackageProvider -Name NuGet -Force
+Install-Module -Name BurntToast -Force
+Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Force
+
+Write-Host "Installing Chocolatey"
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+choco install chocolatey-core.extension -y $cache
+choco feature enable --name=allowGlobalConfirmation
+choco feature enable -n=useRememberedArgumentsForUpgrades
+Write-Host "Done"

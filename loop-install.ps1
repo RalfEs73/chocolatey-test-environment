637bda0a58a000c91e70a023e5b69d19cffbbab7
RalfEs73 <ralf@essbaumer.de>
2021-02-13 23:34:12 +0100

    Update loop-install.ps1

--- loop-install.ps1
+++ loop-install.ps1
@@ -1,7 +1,16 @@
-$package = "touchportal"
-$version = "--version=2.2.005"
+$package = "cloudberrybackup"
+$version = "--version=7.0.0.648"
+
+New-Item -Path "C:\Temp\ChocoCache" -ItemType directory -Force | Out-Null
+$cache = "--cacheLocation=C:\Temp\ChocoCache"
+
+$CurrentUser = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name
+$CurrentUserName = $CurrentUser.split("\")[1]
+
+Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Force
 
 For ($i=1; $i -lt 5; $i++) {
+    Write-Host "*** Durchlauf $i ***" -ForegroundColor Red -BackgroundColor White
 	choco install $package $version --force
 	Start-Sleep 1
 	choco uninstall $package

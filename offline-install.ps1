bce951d46a318e30ff63927717ca71c25fb74629
RalfEs73 <ralf@essbaumer.de>
2021-02-24 19:56:20 +0100

    Update offline-install.ps1

--- offline-install.ps1
+++ offline-install.ps1
@@ -1,11 +1,23 @@
-$gitpackageurl = "https://github.com/RalfEs73/chocolatey-packages/raw/master/filminfo/filminfo.0.6.1.3.nupkg"
-$gitpackage = "filminfo.0.6.1.3.nupkg"
-$package = "filminfo"
+$gitpackageurl = "https://github.com/RalfEs73/chocolatey-packages/raw/master/anystream/anystream.1.0.9.0.nupkg"
+$gitpackage = "anystream.1.0.9.0.nupkg"
+$package = "anystream"
+$hash = "2D9D5F571162C21A57A2E86B07B5909125163C43DD856A4A12FBF5972C856FA7"
 
 $CurrentUser = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name
 $CurrentUserName = $CurrentUser.split("\")[1]
 
 Start-BitsTransfer -Source $gitpackageurl -Destination $gitpackage
-choco install $package -s .
-del $gitpackage
-Write-Host "Done"
+
+if((Get-FileHash $gitpackage).hash -ne $hash)
+    {
+    Write-Host "Falsche Packet Version"
+    del $gitpackage
+    }
+    else
+    {
+    choco install $package -s . -force
+    del $gitpackage
+    Write-Host "Done"
+    choco uninstall $package
+    Write-Host "Done"
+    }

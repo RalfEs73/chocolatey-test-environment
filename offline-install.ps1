8ca1977bcfc2446ce31bac7baf72551957f221bf
RalfEs73 <ralf@essbaumer.de>
2021-03-10 19:50:16 +0100

    Update offline-install.ps1

--- offline-install.ps1
+++ offline-install.ps1
@@ -1,7 +1,7 @@
-$gitpackageurl = "https://github.com/RalfEs73/chocolatey-packages/raw/master/anystream/anystream.1.0.9.0.nupkg"
-$gitpackage = "anystream.1.0.9.0.nupkg"
-$package = "anystream"
-$hash = "2D9D5F571162C21A57A2E86B07B5909125163C43DD856A4A12FBF5972C856FA7"
+$gitpackageurl = "https://github.com/RalfEs73/chocolatey-packages/raw/master/planetmule/planetmule.1.3.6.nupkg"
+$gitpackage = "planetmule.1.3.6.nupkg"
+$package = "planetmule"
+$hash = "1BB29F9B518C91D7BF9B9FA5F7FCC2455B8C0DD54E9E3A741F39184C00F3F7EE"
 
 $CurrentUser = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name
 $CurrentUserName = $CurrentUser.split("\")[1]

6442b8305f4b27ead98a769a14aae3dbfdf3299d
RalfEs73 <ralf@essbaumer.de>
2021-03-18 09:42:53 +0100

    Update loop-install.ps1

--- loop-install.ps1
+++ loop-install.ps1
@@ -1,5 +1,5 @@
 $package = "cloudberrybackup"
-$version = "--version=7.0.0.648"
+$version = "7.0.0.648"
 $loops = 5
 
 New-Item -Path "C:\Temp\ChocoCache" -ItemType directory -Force | Out-Null
@@ -13,7 +13,7 @@ Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Force
 For ($i=0; $i -lt $loops; $i++) {
 	$temp = $i + 1
     Write-Host "*** Durchlauf $temp von $loops ***" -ForegroundColor Red -BackgroundColor White
-	choco install $package $version --force
+	choco install $package --version=$version --force
 	Start-Sleep 1
 	choco uninstall $package
 	Start-Sleep 1	

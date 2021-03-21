9a3e4dc37525d390c091bcd653e5e272f16cc24f
RalfEs73 <ralf@essbaumer.de>
2021-03-21 20:18:36 +0100

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,5 +1,8 @@
 # iex ((New-Object System.Net.WebClient).DownloadString('https://github.com/RalfEs73/chocolatey-test-environment/raw/main/test-install.ps1'))
 
+Start-BitsTransfer -Source "https://github.com/RalfEs73/chocolatey-test-environment/raw/main/loop-install.ps1" -Destination "C:\Users\$CurrentUserName\Desktop\loop-install.ps1"
+Start-BitsTransfer -Source "https://github.com/RalfEs73/chocolatey-test-environment/raw/main/offline-install.ps1" -Destination "C:\Users\$CurrentUserName\Desktop\offline-install.ps1"
+
 New-Item -Path "C:\Temp\ChocoCache" -ItemType directory -Force | Out-Null
 $cache = "--cacheLocation=C:\Temp\ChocoCache"
 
@@ -14,6 +17,3 @@ choco install chocolatey-core.extension -y $cache
 choco feature enable --name=allowGlobalConfirmation
 choco feature enable -n=useRememberedArgumentsForUpgrades
 Write-Host "Done"
-
-Start-BitsTransfer -Source "https://github.com/RalfEs73/chocolatey-test-environment/raw/main/loop-install.ps1" -Destination "C:\Users\$CurrentUserName\Desktop\loop-install.ps1"
-Start-BitsTransfer -Source "https://github.com/RalfEs73/chocolatey-test-environment/raw/main/offline-install.ps1" -Destination "C:\Users\$CurrentUserName\Desktop\offline-install.ps1"

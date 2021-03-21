f5efbdcd333cd81ba084ad6b14060793f40aa5f2
RalfEs73 <ralf@essbaumer.de>
2021-03-21 21:52:59 +0100

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -7,12 +7,13 @@ $CurrentUserName = $CurrentUser.split("\")[1]
 
 Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Force
 
-Start-BitsTransfer -Source "https://github.com/RalfEs73/chocolatey-test-environment/raw/main/loop-install.ps1" -Destination "C:\Users\$CurrentUserName\Desktop\loop-install.ps1"
-Start-BitsTransfer -Source "https://github.com/RalfEs73/chocolatey-test-environment/raw/main/offline-install.ps1" -Destination "C:\Users\$CurrentUserName\Desktop\offline-install.ps1"
-
 Write-Host "Installing Chocolatey"
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 choco install chocolatey-core.extension -y $cache
 choco feature enable --name=allowGlobalConfirmation
 choco feature enable -n=useRememberedArgumentsForUpgrades
+
+Start-BitsTransfer -Source "https://github.com/RalfEs73/chocolatey-test-environment/raw/main/loop-install.ps1" -Destination "C:\Users\$CurrentUserName\Desktop\loop-install.ps1"
+Start-BitsTransfer -Source "https://github.com/RalfEs73/chocolatey-test-environment/raw/main/offline-install.ps1" -Destination "C:\Users\$CurrentUserName\Desktop\offline-install.ps1"
+
 Write-Host "Done"
